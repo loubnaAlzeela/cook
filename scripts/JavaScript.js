f9397edb8c2dc949d22a7747f2fcb48c78685d54
loubnaAlzeela <31747991+loubnaAlzeela@users.noreply.github.com>
2022-02-10 11:57:42 +0200

    Add files via upload

--- scripts/JavaScript.js
+++ scripts/JavaScript.js
@@ -1,1 +1,30 @@
+﻿$(document).ready(function() {
+  // Transition effect for navbar 
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if($(this).scrollTop() > 400) { 
+        $('.navbar').addClass('solid');
+       
+       } else {
+        $('.navbar').removeClass('solid');
+       
+    }
+    if($(this).scrollTop() > 80) { 
+      $('.home-inner').addClass('home-inner_scroll');
+     
+     } else {
+      $('.home-inner').removeClass('home-inner_scroll');
+     
+  }
+
+  });
+
+});
+
+
+function opendetails(){
+window.open('details.html','_self');
+}
+
+
 
